8ad6cc16c4879bc19a4945d56086d7d1002ae21f
AzaleeX <azaleemcpe@gmail.com>
2024-02-13 17:23:35 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,18 @@
 import DefaultLayout from "@/layouts/default";
 import HeroHome from "@/components/home/HeroHome";
 import FeatureHome from "@/components/home/FeatureHome";
+import StatsSection from "@/components/home/StatsSection";
+import FAQSection from "@/components/home/FAQHome";
 
 export default function IndexPage() {
 	return (
 		<DefaultLayout>
-			<HeroHome />
-			<FeatureHome />
+			<div className="bg-hero-pattern bg-no-repeat bg-center bg-cover">
+				<HeroHome />
+				<FeatureHome />
+				<StatsSection />
+				<FAQSection />
+			</div>
 		</DefaultLayout>
 	);
 }
