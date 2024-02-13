53d79637bf2e60c92870a847605b3a0c66718e23
AzaleeX <azaleemcpe@gmail.com>
2024-02-13 17:43:40 +0100

    Update head.tsx

--- layouts/head.tsx
+++ layouts/head.tsx
@@ -5,21 +5,19 @@ import {siteConfig} from "@/config/site";
 export const Head = () => {
 	return (
 		<NextHead>
-			<title>{siteConfig.name}</title>
+			<title>{"PowerNukkitX | Home"}</title>
 			<meta content={siteConfig.description} name="description"/>
 			<meta property="og:title" content={siteConfig.name}/>
 			<meta property="og:description" content={siteConfig.description}/>
 			<meta property="og:site_name" content="PowerNukkitX"/>
-			<meta property="og:image" content="/PNX_banner.png"/>
+			<meta property="og:image" content="/logoPNX.webp"/>
 			<meta property="og:url" content=""/>
 			<meta property="og:type" content="website"/>
 			<meta name="twitter:title" content="PowerNukkitX"/>
 			<meta name="twitter:description" content={siteConfig.description}/>
-			<meta name="twitter:image" content="/PNX_banner.png"/>
-			<meta name="twitter:card" content="summary_large_image"/>
-			<meta name="twitter:site" content="@PowerNukkitX"/>
+			<meta name="twitter:image" content="/logoPNX.webp"/>
 			<meta property="keywords" name="keywords"
-				  content="PowerNukkitX, powernukkitx, powernukkit, minecraft, bedrock, mcpe, mcbe, minecraft-bedrock"/>
+				  content="PowerNukkitX, powernukkitx, powernukkit, minecraft, bedrock, mcpe, mcbe, minecraft-bedrock, software, software-bedrock, nukkit, nukkitx"/>
 			<meta
 				key="viewport"
 				content="viewport-fit=cover, width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
