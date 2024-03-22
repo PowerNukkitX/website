574d43aaed7d1f018379a05c9bb4bb2a7b3bbbc0
AzaleeX <azaleemcpe@gmail.com>
2024-03-22 23:49:55 +0100

    Change in the stats section
    - which correctly displays the right data (stars, servers, or online players).

--- components/home/StatsSection.tsx
+++ components/home/StatsSection.tsx
@@ -1,36 +1,76 @@
-import React from 'react';
-import {Spacer} from "@nextui-org/react";
+import React, {useEffect, useState} from 'react';
+
+async function get(url: string) {
+    const response = await fetch(url);
+    return response.text();
+}
+
+async function refreshPNXServers(callback: (count: number) => void) {
+    let response = await get("https://bstats.org/api/v1/plugins/10277/charts/nukkit_version/data");
+    let data = JSON.parse(response);
+    let count = 0;
+    for (const each of data) {
+        if (each.name.indexOf("PNX") !== -1) {
+            count += each.y;
+        }
+    }
+    response = await get("https://bstats.org/api/v1/plugins/16708/charts/servers/data");
+    data = JSON.parse(response);
+    count += data[data.length - 1][1];
+    if (callback) {
+        callback(count);
+    }
+}
+
+async function refreshPNXPlayers(callback: (count: number) => void) {
+    const response = await get("https://bstats.org/api/v1/plugins/16708/charts/players/data");
+    const data = JSON.parse(response);
+    const count = data[data.length - 1][1];
+    if (callback) {
+        callback(count);
+    }
+}
 
 const StatsSection = () => {
-    const stats = [
-        {
-            data: "213",
-            title: "Totals Users"
-        },
-        {
-            data: "899+",
-            title: "Downloads"
-        },
-        {
-            data: "79",
-            title: "Servers using PowerNukkitX"
-        },
-    ];
+    const [stars, setStars] = useState(0);
+    const [servers, setServers] = useState(0);
+    const [players, setPlayers] = useState(0);
+    const [totalServers, setTotalServers] = useState(0);
+    const [totalPlayers, setTotalPlayers] = useState(0);
+
+    useEffect(() => {
+        fetch('https://api.powernukkitx.cn/v2/git/star')
+            .then(response => response.json())
+            .then(data => setStars(data.star))
+            .catch(error => console.error('Error:', error));
+        refreshPNXServers((count) => {
+            setServers(count);
+            setTotalServers(prevTotal => prevTotal + count);
+        });
+
+        refreshPNXPlayers((count) => {
+            setPlayers(count);
+            setTotalPlayers(prevTotal => prevTotal + count);
+        });
+    }, []);
 
     return (
-        <section className="py-14 p-4 min-h-[350px] flex items-center justify-center font-[sans-serif] text-[#333]">
+        <section id={"stats"} className="py-14 p-4 min-h-[350px] flex items-center justify-center font-[sans-serif] text-[#333]">
             <div
-                className="shadow-[0_4px_24px_-8px_rgba(0,0,0,0.2)] grid lg:grid-cols-3 sm:grid-cols-2 sm:gap-24 gap-12 rounded-3xl px-20 py-10">
+                className="grid lg:grid-cols-3 sm:grid-cols-2 sm:gap-24 gap-12 rounded-xl px-20 py-10 bg-content1">
                 <div className="text-center">
-                    <h3 className="text-4xl font-extrabold text-white">51 <span className="text-success">Servers</span></h3>
+                    <h3 className="text-4xl font-extrabold text-white">{totalServers} <span
+                        className="text-success">Servers</span></h3>
                     <p className="text-gray-300 font-semibold mt-3">are running PowerNukkitX</p>
                 </div>
                 <div className="text-center">
-                    <h3 className="text-4xl font-extrabold text-white">8 <span className="text-success">Players</span></h3>
+                    <h3 className="text-4xl font-extrabold text-white">{totalPlayers} <span
+                        className="text-success">Players</span></h3>
                     <p className="text-gray-300 font-semibold mt-3">are playing on PowerNukkitX</p>
                 </div>
                 <div className="text-center">
-                    <h3 className="text-4xl font-extrabold text-white">384 <span className="text-success">Github Star</span></h3>
+                    <h3 className="text-4xl font-extrabold text-white">{stars} <span className="text-success">Github Star</span>
+                    </h3>
                     <p className="text-gray-300 font-semibold mt-3">on Github</p>
                 </div>
             </div>
