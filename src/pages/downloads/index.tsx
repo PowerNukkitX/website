4a011cf8107d46ab913abaf9615ada028579d92e
AzaleeX <azaleemcpe@gmail.com>
2024-07-21 18:35:34 +0200

    fix(link docs)

--- src/pages/downloads/index.tsx
+++ src/pages/downloads/index.tsx
@@ -28,7 +28,7 @@ export default function Index() {
 
                     <Button
                         as={Link}
-                        href={siteConfig.links.github}
+                        href={siteConfig.links.docs}
                         className="inline-flex items-center justify-center sm:w-auto flex-grow h-12 px-6 group text-sm font-medium ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-content1 transition-all duration-200 ease-in-out hover:ring-2 hover:ring-offset-2 hover:ring-content2"
                     >
                         <DocsIcon/>
